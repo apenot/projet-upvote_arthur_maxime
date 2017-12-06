c5a81bf0c0f78958e4ce05a914381b82a5c5ef51
apenot <arthur.penot@gmail.com>
2017-12-06 09:01:40 +0100

    Création des premiers fichier vue

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pageHome">
+      <h2>Home.vue</h2>
+        <my-personne  v-bind:value="v" v-bind:index="i" v-bind:key="i" v-for="(v, i) in listeCategorie"></my-personne>
+  </div>
+</template>
+
+<script>
+import Categorie from './Categorie';
+export default {
+    data() {
+        return {
+            personnes: this.$root.personnes
+        };
+    },
+    computed: {
+    },
+    methods: {
+    },
+    filters: {
+    },
+    components: {
+        'my-categorie': Categorie
+    }
+};
+</script>
+
+<style>
+
+</style>
