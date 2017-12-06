dba36998a439e1ab6fe84d95906322c07d315fb6
apenot <arthur.penot@gmail.com>
2017-12-06 09:54:28 +0100

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import Vue from 'vue';
+import firebase from 'firebase';
+import VueFire from 'vuefire';
+Vue.use(VueFire);
+// Initialize Firebase
+var config = {
+    apiKey: 'AIzaSyBGlbmoiFO7_LR4BrRRmdEDijewDwdK4Aw',
+    authDomain: 'upvote-cf339.firebaseapp.com',
+    databaseURL: 'https://upvote-cf339.firebaseio.com',
+    projectId: 'upvote-cf339',
+    storageBucket: 'upvote-cf339.appspot.com',
+    messagingSenderId: '481064603440'
+};
+firebase.initializeApp(config);
