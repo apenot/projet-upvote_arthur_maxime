2ffb610b5ede0de8e94093b4b3940a61905ad343
apenot <arthur.penot@gmail.com>
2017-12-07 15:15:09 +0100

    Ajout d'une page pour ajouter des nouvelles catégories

---
+++ src/components/CategorieAjout.vue
@@ -0,0 +1,49 @@
+<template>  
+    <div class="row">    
+        <div class="col-sm-12 col-md-8 col-lg-6 col-lx-4 mx-auto">
+            <h2>Ajouter une categorie</h2>
+            <form>
+                <div class="form-group">
+                    <label for="nomCat">Nom</label>
+                    <input id="nomCat" class="form-control" placeholder="Catégorie" v-model="categorieNom" required>
+                </div>
+                <div class="form-group">
+                    <label for="nomSlug">Slug</label>
+                    <input id="nomSlug" class="form-control" placeholder="Slug" v-model="categorieSlug" required>
+                </div>
+                <button v-on:click="ajoutCategorie()" class="btn btn-primary">Enregistrer</button>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import firebase from '../firebase';
+export default {
+    data() {
+        return {
+            categorieNom: '',
+            categorieSlug: ''
+        };
+    },
+    firebase() {
+        return {
+            categories: {
+                source: firebase.database().ref('/categories')
+            }
+        };
+    },
+    methods: {
+        ajoutCategorie() {
+            this.$firebaseRefs.categories.push({slug: this.categorieSlug, nom: this.categorieNom});
+            this.categorieSlug = '';
+            this.categorieNom = '';
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
