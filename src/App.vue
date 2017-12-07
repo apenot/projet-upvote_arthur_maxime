1b6d72f7b508c3eb6e1a7f68504c7f2f4c51eef7
apenot <arthur.penot@gmail.com>
2017-12-07 15:15:52 +0100

    Ajout de la class container de bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <router-view></router-view>
   </div>
 </template>
@@ -19,7 +19,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
