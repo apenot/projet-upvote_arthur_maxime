e0850aa7964ab5f73b49d33e78be1b9e538cb301
Maxime Parret <emix277@hotmail.com>
2018-01-09 13:55:06 +0100

    Ajout d'onglet dans haut de page + correction responsive design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="pageHome">
-        <h2>Page  Home.vue
-        <router-link :to="{ name: 'CategorieAjout', params: {}}">
-            <button type="button" class="btn btn-outline-success" link="">Ajouter une categorie</button>
-        </router-link>
-        </h2>
         <my-categorie  v-bind:value="v" v-bind:index="i" v-bind:key="i" v-for="(v, i) in categories"></my-categorie>
   </div>
 </template>
