ec15ce8022b469ae2126f1def03d09bbe0587f4f
apenot <arthur.penot@gmail.com>
2018-01-10 08:34:09 +0100

    Affichage de toutes les vidéos dans le détail d'une catégorie

--- src/components/CategorieDetail.vue
+++ src/components/CategorieDetail.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
-      <h2>Detail de la categorie {{categorie.nom}} key: {{categorie['.key']}}</h2>
-      liste des videos
+      <h1>{{categorie.nom}}</h1>
+        <div class="row">
+            <my-video  v-bind:value="v" v-bind:index="i" v-bind:key="i" v-for="(v, i) in videos"></my-video>
+        </div>
   </div>
 </template>
 
 <script>
+import firebase from '../firebase';
+import Video from './Video';
 export default {
+    props: ['value', 'index'],
     computed: {
         categorie() {
             return this.$root.categories.find(
-            c => String(c.slug) === String(this.$route.params.slug)
-        );
+            c => String(c.slug) === String(this.$route.params.slug));
         }
+    },
+    data() {
+        return {
+            videos: []
+        };
+    },
+    mounted() {
+        this.loadData();
+    },
+    watch: {
+        '$root.categories': function () {
+            this.loadData();
+        }
+    },
+    methods: {
+        loadData() {
+            this.$bindAsArray('videos', firebase.database().ref('categories/' + this.categorie['.key'] + '/videos'));    
+        }
+    },
+    components: {
+        'my-video': Video
     }
 };
 </script>
